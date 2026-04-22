001b3ade5b53b649d7da9016d9b5a2ac4c139e05
Websuu <99693033+Websuu@users.noreply.github.com>
2026-04-22 13:07:49 +0200

    Add documentation comments to Logger class

--- src/Logger.h
+++ src/Logger.h
@@ -1,22 +1,41 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+/**
+ * @file Logger.h
+ * @brief Prosty i bezpieczny wątkowo system logowania zdarzeń.
+ * @author Mateusz Kołodziejczak, Maciej Kwiatkowski
+ */
+
 #include <string>
 #include <iostream>
 #include <mutex>
 
-/// @brief A thread-safe logger for application events.
+/**
+ * @class Logger
+ * @brief Statyczna klasa pomocnicza do zapisu zdarzeń i błędów aplikacji.
+ * * Klasa zapewnia synchronizację za pomocą mutexa, co gwarantuje, że logi 
+ * z różnych wątków (np. UI i WorkerThread) nie zostaną wymieszane w konsoli.
+ */
 class Logger {
 public:
-    /// @brief Writes an info message to stdout.
-    /// @param msg The message to log.
+    /**
+     * @brief Zapisuje wiadomość informacyjną do standardowego wyjścia (stdout).
+     * @param msg Treść wiadomości do zalogowania.
+     */
     static void info(const std::string& msg);
 
-    /// @brief Writes an error message to stderr.
-    /// @param msg The error message to log.
+    /**
+     * @brief Zapisuje komunikat o błędzie do standardowego wyjścia błędów (stderr).
+     * @param msg Treść błędu do zalogowania.
+     */
     static void error(const std::string& msg);
 
 private:
+    /**
+     * @brief Mutex synchronizujący dostęp do strumieni wyjściowych.
+     * Zapobiega przeplataniu się komunikatów przy jednoczesnym dostępie z wielu wątków.
+     */
     static std::mutex logMutex;
 };
 
